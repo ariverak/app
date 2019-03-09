d08d3a8ab2fdba214af3ca15fc0eb052cff206ef
ariverak <jrivera.v@outlook.com>
2019-03-09 03:08:47 -0300

    build

---
+++ precache-manifest.240a14efe00be98b5e6d4ebe54766afd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8eb22179b0a78d47bbe709b41e4b6d9c",
+    "url": "/app/static/media/lider-logo.8eb22179.svg"
+  },
+  {
+    "revision": "ccdec12fb3f88b3ed77c",
+    "url": "/app/static/js/runtime~main.d8421c71.js"
+  },
+  {
+    "revision": "b200482717b17abd895f",
+    "url": "/app/static/js/main.5caedefd.chunk.js"
+  },
+  {
+    "revision": "cca702313ea065fdd366",
+    "url": "/app/static/js/2.469ed7e5.chunk.js"
+  },
+  {
+    "revision": "b200482717b17abd895f",
+    "url": "/app/static/css/main.b6e50e28.chunk.css"
+  },
+  {
+    "revision": "cca702313ea065fdd366",
+    "url": "/app/static/css/2.9fc2e11e.chunk.css"
+  },
+  {
+    "revision": "7cf0ae031b9bd9d81094c34a758fe62d",
+    "url": "/app/index.html"
+  }
+];
